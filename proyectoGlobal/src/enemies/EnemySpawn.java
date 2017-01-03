f59a59f9d1159ab2f27fb8b3a50a61774cb8866e
Sergio Rosello <sergio-rosello@hotmail.com>
2017-01-03 20:33:11 +0100

    Cambio en enemySpawn y EnirenomentSelection
     para poder randomizar siempre, incluso la primera vez

--- proyectoGlobal/src/enemies/EnemySpawn.java
+++ proyectoGlobal/src/enemies/EnemySpawn.java
@@ -1,15 +1,19 @@
 package enemies;
 
+import gameLogic.EnvirenomentSelection;
+
 public class EnemySpawn {
 
 	private EnemyFactory enemyFactory;
+	private EnvirenomentSelection envirenomentSelection;
 	
-	public EnemySpawn(EnemyFactory enemyFactory){
-		this.enemyFactory = enemyFactory;
+	public EnemySpawn(){
+		this.enemyFactory = envirenomentSelection.selectEnemyLocation();
 	}
 	
 	//Decide que enemigo crear basándose en una semilla aleatoria.
 	public void createEnemy(){
+		this.enemyFactory = envirenomentSelection.selectEnemyLocation();
 		//Genera un número aleatorio entre 0 y 2.
 		int randomEnemy = (int)Math.random() * 3;
 		if(randomEnemy == 0)enemyFactory.createElf();
--- proyectoGlobal/src/gameLogic/EnvirenomentSelection.java
+++ proyectoGlobal/src/gameLogic/EnvirenomentSelection.java
@@ -5,15 +5,14 @@ import enemies.*;
 public class EnvirenomentSelection {
 
 	EnemyFactory enemyFactory;
-	EnemySpawn enemySpawn;
 	
 	//Decides the enemy's location based on a random seed
-	void selectEnemyLocation(){
+	public EnemyFactory selectEnemyLocation(){
 		int randomEnemyLocation = (int)Math.random() * 3;
 		if(randomEnemyLocation == 0)enemyFactory = new ForestEnemyFactory();
 		else if(randomEnemyLocation == 1) enemyFactory = new TundraEnemyFactory();
 		else enemyFactory = new SwampEnemyFactory();
 		
-		enemySpawn.setEnemyFactory(enemyFactory);
+		return enemyFactory;
 	}
 }
